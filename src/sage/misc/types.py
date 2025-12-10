040cea13160e8bcdd6c3e91e47ac0492a2ee559d
Vincent Macri <vincent.macri@ucalgary.ca>
2025-12-10 15:58:46 -0700

    Formatting

--- src/sage/misc/types.py
+++ src/sage/misc/types.py
@@ -5,6 +5,10 @@ This typing module defines some common aliases to be used for type annotations i
 This module performs many possibly heavy imports, and so it should only be imported
 within an ``if typing.TYPE_CHECKING`` block. This module will raise an error if it
 is imported at runtime.
+
+The purpose of this module is to provide aliases when there are many types that
+easily coerce into another type, or when the same code will give a different type
+depending on whether the preparser is enabled or not.
 """
 # ****************************************************************************
 #       Copyright (C) 2025 Vincent Macri <vincent.macri@ucalgary.ca>
@@ -19,10 +23,10 @@ is imported at runtime.
 import typing
 
 if typing.TYPE_CHECKING:
-    from sage.rings.integer import Integer
     from sage.rings.finite_rings.integer_mod import IntegerMod_abstract
+    from sage.rings.integer import Integer
 
     type Int = Integer | int
-    type IntMod = Int | IntegerMod_abstract
+    type IntMod = IntegerMod_abstract | Int
 else:
-    assert False, 'sage.misc.types cannot be imported at runtime, it may only be imported in an if `typing.TYPE_CHECKING` block
+    assert False, 'sage.misc.types cannot be imported at runtime, it may only be imported in an if `typing.TYPE_CHECKING` block'
